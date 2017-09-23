437b1335256c79afa52f671b598139734465c426
Harry T. Vennik <htvennik@gmail.com>
2017-09-23 13:45:29 +0200

    Improve code formatting

--- source/ddbus/proxy.d
+++ source/ddbus/proxy.d
@@ -1,18 +1,19 @@
 module ddbus.proxy;
 
+import ddbus.thin : Connection, Message, DBusAny;
+import ddbus.util : canDBus, allCanDBus;
+
 import alt.typecons;
 
 import std.conv;
 import std.string;
 import std.traits;
 
-import ddbus.thin : Connection, Message, DBusAny;
-import ddbus.util : canDBus, allCanDBus;
-
 /++
     UDA to set the DBus namespace of an interface
   +/
-auto namespace(string nsName) {
+auto namespace(string nsName)
+{
     return NamespaceAttribute(nsName);
 }
 
@@ -22,7 +23,8 @@ auto namespace(string nsName) {
     A signal binder must have exactly one parameter, which must be a delegate
     type with a parameter list that matches the signal.
   +/
-auto signal() @property {
+auto signal() @property
+{
     return SignalAttribute.init;
 }
 
@@ -72,10 +74,18 @@ template dbusNameOf(alias I)
   enum dbusNameOf = getUDAs!(I, NamespaceAttribute)[0].name ~ '.' ~ I.stringof;
 }
 
-class RemoteException : Exception {
-  this(Proxy proxy, string method, Message msg)
-  in { assert (msg.isError); }
-  body {
+class RemoteException : Exception
+{
+  this(
+    Proxy proxy,
+    string method,
+    Message msg)
+  in
+  {
+    assert (msg.isError);
+  }
+  body
+  {
     auto errorStr = "Exception thrown by service: " ~ errorName;
 
     if (msg.signature[0] == 's') {
@@ -86,14 +96,16 @@ class RemoteException : Exception {
     _msg = msg;
   }
 
-  string errorName() @property const nothrow {
+  string errorName() @property const nothrow
+  {
     return _msg.errorName;
   }
 
   /**
     Get the DBus error message represented by this exception.
   */
-  const(Message) message() @property const nothrow @safe {
+  const(Message) message() @property const nothrow @safe
+  {
     return _msg;
   }
 
@@ -103,7 +115,8 @@ class RemoteException : Exception {
 
 abstract class Proxy
 {
-  this(Connection conn, string dest, string path) {
+  this(Connection conn, string dest, string path)
+  {
     this._conn = conn;
     this._dest = dest.toStringz();
     this._path = path.toStringz();
@@ -113,7 +126,8 @@ abstract class Proxy
   abstract const(char)* iface() const nothrow @property @safe;
 
   Ret _call(Ret, Args...)(string meth, Args args)
-    if (allCanDBus!Args && (is(Ret == void) || canDBus!Ret)) {
+    if (allCanDBus!Args && (is(Ret == void) || canDBus!Ret))
+  {
     import ddbus.c_lib : dbus_message_new_method_call;
 
     Message msg = Message(
@@ -161,13 +175,20 @@ abstract class Proxy
 
 class DynamicProxy : Proxy
 {
-  this(Connection conn, string dest, string path, string iface) {
+  this(
+    Connection conn,
+    string dest,
+    string path,
+    string iface)
+  {
     super(conn, dest, path);
+
     _iface = iface.toStringz();
   }
 
   // TODO: use known method signature
-  DBusAny opDispatch(string meth, Args...)(Args args) {
+  DBusAny opDispatch(string meth, Args...)(Args args)
+  {
     auto ret = _call!Message(meth, args).readTuple!DBusAny();
 
     if (ret.tuple.length == 1)
@@ -177,7 +198,8 @@ class DynamicProxy : Proxy
   }
 
   protected:
-  override const(char)* iface() const nothrow @property @safe {
+  override const(char)* iface() const nothrow @property @safe
+  {
     return _iface;
   }
 
@@ -186,7 +208,8 @@ class DynamicProxy : Proxy
 }
 
 I createProxy(I)(Connection conn, string dest, string path)
-  if (isDBusInterface!I) {
+  if (isDBusInterface!I)
+{
   return new StaticProxy!I(conn, dest, path);
 }
 
@@ -210,12 +233,17 @@ alias StaticProxy(I) = AutoImplement!(I, StaticProxyBase, generateDBusProxy);
 
 class StaticProxy(I) : AutoImplement!(I, Proxy, generateDBusProxy)
 {
-  this(Connection conn, string dest, string path) {
+  this(
+    Connection conn,
+    string dest,
+    string path)
+  {
     super(conn, dest, path);
   }
 
   protected:
-  override const(char)* iface() const nothrow @property @safe {
+  override const(char)* iface() const nothrow @property @safe
+  {
     return &dbusNameOf!I[0]; // always a literal, thus \0 terminated
   }
 }
@@ -227,7 +255,8 @@ struct NamespaceAttribute
 
 struct SignalAttribute { }
 
-template generateDBusProxy(I, alias fun) {
+template generateDBusProxy(I, alias fun)
+{
   static if (isDBusMethod!fun)
     alias generateMethodProxy!(I, fun) generateDBusProxy;
   else static if (isDBusSignal!fun)
@@ -236,7 +265,8 @@ template generateDBusProxy(I, alias fun) {
     static assert (false);
 }
 
-template countOutParameters(alias fun, size_t i = Parameters!fun.length - 1) {
+template countOutParameters(alias fun, size_t i = Parameters!fun.length - 1)
+{
   static if (Parameters!fun.length == 0)
     enum countOutParameters = 0;
   else {
@@ -259,7 +289,8 @@ template countOutParameters(alias fun, size_t i = Parameters!fun.length - 1) {
   }
 }
 
-template countInParameters(alias fun, size_t i = 0) {
+template countInParameters(alias fun, size_t i = 0)
+{
   static if (Parameters!fun.length == 0)
     enum countInParameters = 0;
   else {
@@ -279,97 +310,106 @@ template countInParameters(alias fun, size_t i = 0) {
   }
 }
 
-string ucfirst(string s) {
+string ucfirst(string s)
+{
   import std.exception : assumeUnique;
   return assumeUnique([ cast(char) toUpper(s[0]) ]) ~ s[1 .. $];
 }
 
 public {
-string generateMethodProxy(I, alias fun)() @property {
-  import std.traits;
+  // Functions in this block are public for technical reasons only.
+  // They are NOT part of the API
 
-  // Identifier of method, with first letter uppercased
-  enum string meth = ucfirst(__traits(identifier, fun));
+  string generateMethodProxy(I, alias fun)() @property
+  {
 
-  alias ParameterIdentifierTuple!fun argNames;
-  alias ParameterStorageClassTuple!fun argStors;
-  alias ParameterStorageClass STC;
+    import std.traits;
 
-  static assert(argNames.length == argStors.length);
+    // Identifier of method, with first letter uppercased
+    enum string meth = ucfirst(__traits(identifier, fun));
 
-  string inArgsList;
-  size_t outArgsCount;
-  foreach (i, argStor; argStors) {
-    static if (!(argStor & STC.out_)) {
-      inArgsList ~= ", a" ~ i.to!string;
+    alias ParameterIdentifierTuple!fun argNames;
+    alias ParameterStorageClassTuple!fun argStors;
+    alias ParameterStorageClass STC;
 
-      enum bool isNonConstRef =
-        (argStor & STC.ref_) && !is(Parameters!fun[i] == const);
+    static assert(argNames.length == argStors.length);
 
-//      static assert(!outArgsCount || isNonConstRef,
-//        "DBus method must list output parameters after input parameters.");
+    string inArgsList;
+    size_t outArgsCount;
+    foreach (i, argStor; argStors) {
+      static if (!(argStor & STC.out_)) {
+        inArgsList ~= ", a" ~ i.to!string;
 
-      static if (!isNonConstRef)
-        continue;
+        enum bool isNonConstRef =
+          (argStor & STC.ref_) && !is(Parameters!fun[i] == const);
+
+        static assert(!outArgsCount || isNonConstRef,
+          "DBus method must list output parameters after input parameters.");
+
+        static if (!isNonConstRef)
+          continue;
+      }
+
+      ++outArgsCount;
     }
 
-//    ++outArgsCount;
-  }
+    alias ReturnType!fun R;
+    enum haveReturn = !is(R == void);
+    enum string argList = "(\"" ~ meth ~ "\", args[0 .. " ~ countInParameters!fun.to!string  ~ "])";
 
-  alias ReturnType!fun R;
-  enum haveReturn = !is(R == void);
-  enum string argList = "(\"" ~ meth ~ "\", args[0 .. " ~ countInParameters!fun.to!string  ~ "])";
-
-  if (!outArgsCount)
-    static if (haveReturn)
-      return "return this._call!(typeof(return))" ~ argList ~ ';';
+    if (!outArgsCount)
+      static if (haveReturn)
+        return "return this._call!(typeof(return))" ~ argList ~ ';';
+      else
+        return "this._call!(void)" ~ argList ~ ';';
     else
-      return "this._call!(void)" ~ argList ~ ';';
-  else
+    {
+      auto code = "
+        import ddbus.thin : Message;
+        auto msg = this._call!(Message)" ~ argList ~ ";
+
+        alias Tuple!(" ~ (haveReturn ? "typeof(return), " : "")
+          ~ "Parameters!self[$ - " ~ countOutParameters!fun.to!string ~ " .. $]"
+          ~ ") OutTuple;
+
+        auto tup = msg.readTuple!OutTuple();";
+
+      foreach (i, argName; argNames[$ - countOutParameters!fun .. $])
+        code ~= argName ~ " = tup[" ~ (haveReturn ? i + 1 : i).to!string ~ "];";
+
+      static if (haveReturn)
+        code ~= "return tup[0];";
+
+      return code;
+    }
+  }
+
+  string generateSignalProxy(I, alias fun)() @property
   {
-    auto code = "
-      import ddbus.thin : Message;
-      auto msg = this._call!(Message)" ~ argList ~ ";
 
-      alias Tuple!(" ~ (haveReturn ? "typeof(return), " : "")
-        ~ "Parameters!self[$ - " ~ countOutParameters!fun.to!string ~ " .. $]"
-        ~ ") OutTuple;
+    import std.traits;
 
-      auto tup = msg.readTuple!OutTuple();";
+    alias ParameterStorageClassTuple!fun funArgStors;
+    alias ParameterStorageClass STC;
 
-    foreach (i, argName; argNames[$ - countOutParameters!fun .. $])
-      code ~= argName ~ " = tup[" ~ (haveReturn ? i + 1 : i).to!string ~ "];";
+    static assert(funArgStors.length == 1
+      && !(funArgStors[0] & (STC.ref_ | STC.out_))
+      && is(Parameters!fun[0] == delegate),
+      "Signal binding method must have exactly one parameter, "
+      ~ "which must be a delegate.");
 
-    static if (haveReturn)
-      code ~= "return tup[0];";
+    alias ParameterStorageClassTuple!(Parameters!fun[0]) argStors;
 
-    return code;
+    foreach (i, argStor; argStors)
+      static assert (!(argStor & (STC.out_ | STC.ref_)),
+        "Signal handler cannot have out or ref parameters.");
+
+    return "this._bind(\"" ~ __traits(identifier, fun)[2..$] ~ "\", a0);";
   }
 }
 
-string generateSignalProxy(I, alias fun)() @property {
-  import std.traits;
-
-  alias ParameterStorageClassTuple!fun funArgStors;
-  alias ParameterStorageClass STC;
-
-  static assert(funArgStors.length == 1
-    && !(funArgStors[0] & (STC.ref_ | STC.out_))
-    && is(Parameters!fun[0] == delegate),
-    "Signal binding method must have exactly one parameter, "
-    ~ "which must be a delegate.");
-
-  alias ParameterStorageClassTuple!(Parameters!fun[0]) argStors;
-
-  foreach (i, argStor; argStors)
-    static assert (!(argStor & (STC.out_ | STC.ref_)),
-      "Signal handler cannot have out or ref parameters.");
-
-  return "this._bind(\"" ~ __traits(identifier, fun)[2..$] ~ "\", a0);";
-}
-}
-
-unittest {
+unittest
+{
   import ddbus.interfaces : DBus, RequestNameFlags, RequestNameReply;
   import ddbus.bus : BusService, BusPath;
   import ddbus.thin : connectToBus;
